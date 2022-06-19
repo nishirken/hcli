4ff83edb4e85f4f03dc42a31ef788f48c3d25bb7
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2022-06-19 23:53:40 +0400

    Add materialization hash, but currently it doesn't work with flakes.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-linux" ] (system:
     let
       packageName = "hcli";
       overlays = [ haskellNix.overlay
@@ -27,7 +27,9 @@
                     "hpc" "mtl" "parsec" "process" "text" "time" "transformers"
                     "unix" "xhtml" "terminfo"
                   ];
-              }];
+                }];
+              index-state = "2022-03-27T00:00:00Z";
+              plan-sha256 = "1kl5rmvr19j4b3lw34s3i86g6wgdwd9k38yq1krbj5dqsr7hyy5j";
               shell = {
                 tools = {
                   cabal = "3.6.2.0";
@@ -43,8 +45,10 @@
       ];
       pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
       flake = pkgs.${packageName}.flake {};
-    in flake // {
+    in flake // {      
       # Built by `nix build .`
       defaultPackage = flake.packages."${packageName}:exe:${packageName}";
+      # nix build .#calculateMaterializedSha.x86_64-linux --json | jq '.[]|outputs.out' | bash
+      calculateMaterializedSha = pkgs.${packageName}.plan-nix.passthru.calculateMaterializedSha;
     });
 }
