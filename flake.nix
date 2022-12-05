b110564865ac7884795b5aed086059d0905cb94b
Dmitriy Skurikhin <dmitrii.sk@gmail.com>
2022-12-05 17:51:20 +0400

    ghc925

--- flake.nix
+++ flake.nix
@@ -1,4 +1,9 @@
 {
+  nixConfig = {
+    extra-substituters = "https://cache.iog.io";
+    extra-trusted-public-keys = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
+  };  
+
   inputs = {
     haskellNix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
@@ -6,7 +11,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-linux" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
       packageName = "hcli";
       overlays = [ haskellNix.overlay
@@ -15,27 +20,13 @@
           ${packageName} =
             final.haskell-nix.cabalProject' {
               src = ./.;
-              compiler-nix-name = "ghc8107";
-              modules = [{
-                # reinstall only cabal, exclude it from ghc reinstall
-                nonReinstallablePkgs =
-                  [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
-                    "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
-                    "ghcjs-prim" "ghcjs-th" "ghc-boot"
-                    "ghc" "Win32" "array" "binary" "bytestring" "containers"
-                    "directory" "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
-                    "hpc" "mtl" "parsec" "process" "text" "time" "transformers"
-                    "unix" "xhtml" "terminfo"
-                  ];
-                }];
-              index-state = "2022-03-27T00:00:00Z";
-              plan-sha256 = "1kl5rmvr19j4b3lw34s3i86g6wgdwd9k38yq1krbj5dqsr7hyy5j";
+              compiler-nix-name = "ghc925";
               shell = {
                 tools = {
-                  cabal = "3.6.2.0";
+                  cabal = "3.8.1.0";
                   hlint = "latest";
                   haskell-language-server = "latest";
-                  ormolu = "0.1.4.1";
+                  ormolu = "0.5.0.1";
                 };
                 buildInputs = [pkgs.haskellPackages.implicit-hie];
                 withHoogle = true;
